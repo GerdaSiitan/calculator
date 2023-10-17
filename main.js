23a4575a73469eed4f83fb1950a9e170a835471e
Gerda Siitan <gerda.siitan13@gmail.com>
2023-10-17 23:01:24 +0300

    jehuu

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
-const argv = yargs(hideBin(process.argv)).argv;
-console.log(argv.value1 + ' ' + argv.sign + ' ' + argv.value2 + ' = ' );
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    'value1': {
+      describe: '1',
+      type: 'number',
+    },
+    'value2': {
+      describe: '2',
+      type: 'number',
+    }
+  })
+  .help()
+  .argv;
+
+    const result = argv.value1 + argv.value2;
+    console.log(`Result: ${result}`);
+
+yargs.parse();
+
+//ma ei tea , mis ma tegin, vähemalt ma sain aru mis ma kirjutasin ning vähemalt see töötab.
