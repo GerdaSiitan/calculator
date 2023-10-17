ed7741cd26f4260207b12d27de6c212084ab25f8
Gerda Siitan <gerda.siitan13@gmail.com>
2023-10-17 22:42:09 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+const argv = yargs(hideBin(process.argv)).argv;
+console.log(argv.value1 + ' ' + argv.sign + ' ' + argv.value2 + ' = ' );
